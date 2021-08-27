4bce1c1d91913e88e1a17b29b7922b2eb0bb22c1
Julia Cinel <49698954+jcinel@users.noreply.github.com>
2021-08-27 17:54:15 -0300

    Comentário descrevendo funções e atributos

--- Trabalho 1/Scanner/src/main/java/br/ufscar/dc/compiladores/scanner/Utils.java
+++ Trabalho 1/Scanner/src/main/java/br/ufscar/dc/compiladores/scanner/Utils.java
@@ -8,6 +8,7 @@ import java.util.Map;
 import java.util.Set;
 
 public class Utils {
+    /* Conjunto que representa as palavras-chave e símbolos especiais da linguagem. */
     private static final Set<Integer> languageWords = new HashSet<>() {{
         add(AlgoritmicaScanner.PALAVRA_CHAVE);
         add(AlgoritmicaScanner.TIPO);
@@ -28,12 +29,14 @@ public class Utils {
         add(AlgoritmicaScanner.FECHA_COUCHETE);
     }};
 
+    /* Conjunto que representa os tokens de erro da linguagem. */
     private static final Map<Integer, String> errorTokens = new HashMap<>() {{
         put(AlgoritmicaScanner.COMENTARIO_NAO_FECHADO, "comentario nao fechado");
         put(AlgoritmicaScanner.CADEIA_NAO_FECHADA, "cadeia literal nao fechada");
         put(AlgoritmicaScanner.CARACTER_INVALIDO, "simbolo nao identificado");
     }};
 
+    /* Função que recebe um Token e retorna sua forma em String apropriada. */
     public static String stringify(Token tk) {
         if (Utils.isError(tk.getType())) {
             return stringifyError(tk);
@@ -58,6 +61,7 @@ public class Utils {
         return tkString.toString();
     }
 
+    /* Função que recebe um Token de erro e o formata de forma especial. */
     public static String stringifyError(Token tk) {
         StringBuilder tkString = new StringBuilder();
         tkString.append("Linha ");
@@ -74,6 +78,8 @@ public class Utils {
         return tkString.toString();
     }
 
+    /* Função que recebe um tipo de Token e retorna se esse tipo está 
+       presente no conjunto de token de erro. */
     public static Boolean isError(int tkType) {
         return errorTokens.containsKey(tkType);
     }
